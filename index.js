ed01140b9d30d898f8d28d63960b905793164a49
Hritik-Tembhe <84648355+Hritik-Tembhe@users.noreply.github.com>
2021-06-03 14:53:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index9.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App18 from './App18';
+ReactDOM.render(
+  <>
+  <App18/>
+  </>,
+  document.getElementById('root')
+);
